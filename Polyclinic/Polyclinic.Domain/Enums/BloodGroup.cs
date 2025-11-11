e2060fe4d447f2538de0005de68f0dc35e02dcf2
Egor <derasoft004@gmail.com>
2025-11-11 12:34:04 +0400

    commit 21: add comments to Patient's blood groups

--- Polyclinic/Polyclinic.Domain/Enums/BloodGroup.cs
+++ Polyclinic/Polyclinic.Domain/Enums/BloodGroup.cs
@@ -5,8 +5,23 @@ namespace Polyclinic.Domain.Enums;
 /// </summary>
 public enum BloodGroup
 {
+    /// <summary>
+    /// Patient's blood group O
+    /// </summary>
     O,
+    
+    /// <summary>
+    /// Patient's blood group A
+    /// </summary>
     A,
+    
+    /// <summary>
+    /// Patient's blood group B
+    /// </summary>
     B,
+    
+    /// <summary>
+    /// Patient's blood group Ab
+    /// </summary>
     Ab
 }
