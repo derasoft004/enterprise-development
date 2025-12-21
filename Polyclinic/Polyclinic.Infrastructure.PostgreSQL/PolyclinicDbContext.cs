1d6ce76cefc8fbc713fbcd5d8a5073b60cfa0c77
Egor <derasoft004@gmail.com>
2025-12-21 12:26:33 +0400

    commit 39: add postgre context

---
+++ Polyclinic/Polyclinic.Infrastructure.PostgreSQL/PolyclinicDbContext.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Polyclinic.Domain.Subjects;
+
+namespace Polyclinic.Infrastructure.PostgreSQL;
+
+public class PolyclinicDbContext(DbContextOptions<PolyclinicDbContext> options) : DbContext(options)
+{
+    /// <summary>
+    /// Patients table
+    /// </summary>
+    public DbSet<Patient> Patients { get; set; }
+    
+    /// <summary>
+    /// Doctors table
+    /// </summary>
+    public DbSet<Doctor> Doctors { get; set; }
+    
+    /// <summary>
+    /// Appointments table
+    /// </summary>
+    public DbSet<Appointment> Appointments { get; set; }
+    
+    /// <summary>
+    /// Specializations table
+    /// </summary>
+    public DbSet<Specialization> Specializations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // Apply configurations
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PolyclinicDbContext).Assembly);
+    }
+}
