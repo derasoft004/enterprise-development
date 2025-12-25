fdb1eea4678d775b3d9cf01101eab11f8575d8f6
Egor <derasoft004@gmail.com>
2025-12-25 11:30:24 +0400

    commit 52: configurated launch kafka ui and fix aspire

---
+++ Polyclinic/Polyclinic.AppHost/Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder
+    .AddPostgres("postgres")
+    .AddDatabase("polyclinic");
+
+var kafka = builder
+    .AddKafka("kafka")
+    .WithKafkaUI();
+
+builder.AddProject<Projects.Polyclinic_API_Host>("api")
+    .WithReference(postgres)
+    .WithReference(kafka);
+
+builder.AddProject<Projects.Polyclinic_Kafka>("kafka-generator")
+    .WithReference(kafka);
+
+builder.Build().Run();
